f55d9ead1b9b126fdca274b16afa0cd59d8db1c1
sebbonda <141277893+sebbonda@users.noreply.github.com>
2023-08-03 20:11:45 +0000

    WhisperAI Test 2

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import whisper
 
 app = Flask(__name__)
 
-#model = whisper.load_model("base")
-#result = model.transcribe("harvard.wav", verbose=True)
-
-#print(result['text'])
 
 @app.route("/")
 def hello_world():
     return render_template('home.html')
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+#if __name__ == "__main__":
+#  app.run(host='0.0.0.0', debug=True)
+
+print('Test')
+model = whisper.load_model("base")
+print('Test2')
+result = model.transcribe("harvard.wav", verbose=True)
+print('Test3')
+#print(result['text'])
